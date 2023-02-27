4c1b7d56758bbe9d0d8cc989f533ea31bbcbb637
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-02-27 17:44:35 -0300

    -- Errores --

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,12 +1,31 @@
-import { useNavigate, Form } from "react-router-dom";
+import { useNavigate, Form, useActionData } from "react-router-dom";
+import Error from "../components/Error";
 import Formulario from "../components/Formulario";
 export async function action({ request }) {
-  const formData = await request.formData()
-  const datos = Object.fromEntries(formData)
-  return datos
+  const formData = await request.formData();
+  const datos = Object.fromEntries(formData);
+  const errores = [];
+  const email = formData.get("email");
+  let regex = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  );
+  if (!regex.test(email)) {
+    errores.push("El correo no es valido");
+  }
+  if (Object.values(datos).includes("")) {
+    errores.push("Todos los campos son obligatorios");
+  }
+  if (errores) {
+    console.log(errores)
+    return errores;
+  }
+
+
+  return datos;
 }
 const NuevoCliente = () => {
   const navigate = useNavigate();
+  const errores = useActionData();
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
@@ -22,12 +41,13 @@ const NuevoCliente = () => {
         </button>
       </div>
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
-        <Form method="post">
+        {errores?.length && errores.map((errorInfo,index) => <Error key={index}>{errorInfo}</Error>)}
+        <Form method="post" noValidate>
           <Formulario />
           <input
             type="submit"
             className={
-              "mt-5 w-full p-3 bg-blue-800 uppercase font-bold text-white text-lg text-center "
+              "mt-5 w-full p-3 bg-blue-800 uppercase font-bold text-white text-lg text-center cursor-pointer"
             }
             value={"Agregar Cliente"}
           />
